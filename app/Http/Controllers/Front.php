259b7013cb70223be2c628fcb6bd4ada9e4b2528
Vikram Ponuganti <vik@teamdevx.com>
2016-07-16 11:55:29 +0530

    created login/logout/register

---
+++ app/Http/Controllers/Front.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Requests;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Redirect;
+
+
+class Front extends Controller
+{
+    //
+
+    public function login() {
+    	return view('login', array('title' => 'Welcome', 'description' => '', 'page' => 'home'));
+
+}
+    public function register() {
+
+    if (Request::isMethod('post')) {
+        User::create([
+                    'name' => Request::get('name'),
+                    'email' => Request::get('email'),
+                    'password' => bcrypt(Request::get('password')),
+        ]);
+    } 
+    
+    return Redirect::away('login');
+}
+
+
+public function authenticate() {
+    if (Auth::attempt(['email' => Request::get('email'), 'password' => Request::get('password')])) {
+        return redirect()->intended('myaccount');
+        //return view('myaccount', array('title' => 'Srinu', 'description' => '', 'page' => 'home'));
+
+    } else {
+        return view('login', array('title' => 'Welcome', 'description' => '', 'page' => 'home'));
+    }
+}
+
+public function logout() {
+    Auth::logout();
+    
+    return Redirect::away('login');
+}
+
+}
